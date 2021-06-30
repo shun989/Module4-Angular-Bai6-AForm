15d71ef85f40ee3140e5124b65891032e0fa750e
kibo <minhtuanvt89@gmail.com>
2021-06-30 10:13:26 +0700

    validation input

--- demo-validation/src/app/app.component.ts
+++ demo-validation/src/app/app.component.ts
@@ -1,79 +1,122 @@
-import { Component } from '@angular/core';
-import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
+import {Component, OnInit} from '@angular/core';
+import {FormGroup, FormControl, FormBuilder, Validators} from '@angular/forms';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  loginForm: FormGroup;
+export class AppComponent implements OnInit{
+
+  loginForm: FormGroup | undefined;
+  submitted = false;
 
   error_messages = {
     'fname': [
-      { type: 'required', message: 'First Name is required.' },
+      {type: 'required', message: 'First Name is required.'},
     ],
 
     'lname': [
-      { type: 'required', message: 'Last Name is required.' }
+      {type: 'required', message: 'Last Name is required.'},
     ],
 
     'email': [
-      { type: 'required', message: 'Email is required.' },
-      { type: 'minlength', message: 'Email length.' },
-      { type: 'maxlength', message: 'Email length.' },
-      { type: 'required', message: 'please enter a valid email address.' }
+      {type: 'required', message: 'Email is required.'},
+      {type: 'minlength', message: 'Email length.'},
+      {type: 'maxlength', message: 'Email length.'},
+      {type: 'required', message: 'please enter a valid email address.'},
     ],
 
     'password': [
-      { type: 'required', message: 'password is required.' },
-      { type: 'minlength', message: 'password length.' },
-      { type: 'maxlength', message: 'password length.' }
+      {type: 'required', message: 'password is required.'},
+      {type: 'minlength', message: 'password length.'},
+      {type: 'maxlength', message: 'password length.'},
     ],
     'confirmpassword': [
-      { type: 'required', message: 'password is required.' },
-      { type: 'minlength', message: 'password length.' },
-      { type: 'maxlength', message: 'password length.' }
+      {type: 'required', message: 'password is required.'},
+      {type: 'minlength', message: 'password length.'},
+      {type: 'maxlength', message: 'password length.'},
     ],
   }
 
-  constructor(
-    public formBuilder: FormBuilder
-  ) {
-    this.loginForm = this.formBuilder.group({
-      fname: new FormControl('', Validators.compose([
-        Validators.required
-      ])),
-      lname: new FormControl('', Validators.compose([
-        Validators.required
-      ])),
-      email: new FormControl('', Validators.compose([
-        Validators.required,
-        Validators.minLength(6),
-        Validators.maxLength(30)
-      ])),
-      password: new FormControl('', Validators.compose([
-        Validators.required,
-        Validators.minLength(6),
-        Validators.maxLength(30)
-      ])),
-      confirmpassword: new FormControl('', Validators.compose([
-        Validators.required,
-        Validators.minLength(6),
-        Validators.maxLength(30)
-      ])),
-    }, {
-      validators: this.password.bind(this)
-    });
+  constructor(private formBuilder: FormBuilder) {
+
   }
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.loginForm = this.formBuilder.group(
+      {
+        fname: new FormControl('',
+           [
+             Validators.required,
+             Validators.minLength(2),
+           ]
+          ),
+        lname: new FormControl('',
+           [
+             Validators.required,
+             Validators.minLength(2),
+           ]
+          ),
+        email: new FormControl('',
+           [
+             Validators.required,
+             Validators.email,
+             Validators.minLength(6),
+             Validators.maxLength(30)
+           ]
+          ),
+        password: new FormControl('',
+           [
+             Validators.required,
+             Validators.minLength(6),
+             Validators.maxLength(30)
+           ]
+          ),
+        confirmpassword: new FormControl('',
+            [
+              Validators.required,
+              Validators.minLength(6),
+              Validators.maxLength(30)
+            ]
+          ),
+      }, {
+        validators: this.passwordMatch.bind(this)
+      });
   }
 
-  password(formGroup: FormGroup) {
-    const { value: password } = formGroup.get('password');
-    const { value: confirmPassword } = formGroup.get('confirmpassword');
-    return password === confirmPassword ? null : { passwordNotMatch: true };
+  submit() {
+    this.submitted = true;
+    // @ts-ignore
+    let data = this.loginForm.value;
+    console.log(data)
   }
 
+  passwordMatch(formGroup: FormGroup) {
+    // @ts-ignore
+    const {value: password} = formGroup.get('password');
+    // @ts-ignore
+    const {value: confirmPassword} = formGroup.get('confirmpassword');
+    return password === confirmPassword ? null : {passwordNotMatch: true};
+  }
+
+  get fname(){
+    return this.loginForm?.get('fname')
+  }
+
+  get lname(){
+    return this.loginForm?.get('lname')
+  }
+
+  get email(){
+    return this.loginForm?.get('email')
+  }
+
+  get password(){
+    return this.loginForm?.get('password')
+  }
+
+  get confirmpassword(){
+    return this.loginForm?.get('confirmpassword')
+  }
 }
